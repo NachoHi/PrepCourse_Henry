f9bc2df104add25d66b03f933521a92bb6dff272
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-20 16:54:49 -0600

    Ejercicio 10 resuelto

--- C6 Bucles/ejercicios/10.js
+++ C6 Bucles/ejercicios/10.js
@@ -2,6 +2,19 @@ function esDiezOCinco(num) {
   // Retornar true si "num" es 10 o 5.
   // De lo contrario, retornar false.
   // Tu código:
+
+  switch(num){
+    case 5:
+      return true;
+      break;
+
+    case 10:
+      return true;
+      break;
+
+    default:
+      return false;
+  }
 }
 
 module.exports = esDiezOCinco;
