7fc71588e65bb73c5881cbb28c560755c920d84a
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-20 18:45:55 -0600

    Ejercicio 25 resuelto

--- C6 Bucles/ejercicios/25.js
+++ C6 Bucles/ejercicios/25.js
@@ -5,6 +5,20 @@ function esPalindromo(string) {
    // IMPORTANTE: Un palíndromo es una palabra o frase
    // que se lee igual hacia adelante que hacia atrás.
    // Tu código:
+
+   let string2 = '';
+   
+   for (let i = string.length - 1; i >= 0; i--) {
+      string2 += string[i];
+   }
+
+   if (string === string2){
+      return true;
+   }
+   else{
+      return false;
+   }
+
 }
 
 module.exports = esPalindromo;
