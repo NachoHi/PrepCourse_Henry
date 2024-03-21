81af59275825cd57648453b61dd75ed19afa6caa
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-21 17:56:21 -0600

    Ejercicio 01 resuelto

--- C7 Arrays/ejercicios/01.js
+++ C7 Arrays/ejercicios/01.js
@@ -1,6 +1,11 @@
 function devolverPrimerElemento(array) {
   // Retornar el primer elemento del arreglo recibido.
   // Tu código:
+
+  var primero = array.shift();
+
+  return primero;
+  
 }
 
 module.exports = devolverPrimerElemento;
