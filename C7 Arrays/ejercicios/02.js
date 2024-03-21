b472167868ad0a475e8754f7d4d2427d803ccada
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-21 17:57:58 -0600

    Ejercicio 02 resuelto

--- C7 Arrays/ejercicios/02.js
+++ C7 Arrays/ejercicios/02.js
@@ -1,6 +1,10 @@
 function devolverUltimoElemento(array) {
   // Retornar el último elemento del arreglo recibido.
   // Tu código:
+
+  var ultimo = array.pop();
+
+  return ultimo;
 }
 
 module.exports = devolverUltimoElemento;
