5b776fa9857a045e94b244c4164ca2c3bd6abce1
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-20 18:26:01 -0600

    Ejercicio 22 resuelto

--- C6 Bucles/ejercicios/22.js
+++ C6 Bucles/ejercicios/22.js
@@ -6,6 +6,28 @@ function combine(str1, str2, str3) {
    // combine("abc", "", "123") == "a1b2c3"
    // combine("abc", "12345", "") == "a1b2c345"
    // combine("abc", "12345", "67") == "a16b27c345"
+
+   var resultado = "";
+   var aux1 = 0, aux2 = 0, aux3 = 0;
+
+   while(aux1 < str1.length || aux2 < str2.length || aux3 < str3.length){
+      if (str1[aux1]){
+         resultado += str1[aux1];
+         aux1++;
+      }
+
+      if (str2[aux2]){
+         resultado += str2[aux2];
+         aux2++;
+      }
+
+      if (str3[aux3]){
+         resultado += str3[aux3];
+         aux3++;
+      }
+   }
+
+   return resultado;
 }
 
 module.exports = combine;
