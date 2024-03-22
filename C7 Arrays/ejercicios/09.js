4073a1505cf6d3fbd538641d523ae95f32d0a90c
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-22 08:11:41 -0600

    Ejercicio 09 resuelto

--- C7 Arrays/ejercicios/09.js
+++ C7 Arrays/ejercicios/09.js
@@ -2,6 +2,12 @@ function obtenerElementoAleatorio(array) {
    // Devuelve un elemento aleatorio del arreglo array.
    // PISTA: Usa el método Math.random().
    // Tu código:
+
+   var aleatorio = 0;
+
+   aleatorio = parseInt(Math.random(0, array.length-1));
+
+   return array[aleatorio];
 }
 
 module.exports = obtenerElementoAleatorio;
