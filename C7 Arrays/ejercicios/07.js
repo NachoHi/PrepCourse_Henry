2b238fe97a6399d8f5630298ae0092f1e0b8a42c
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-22 07:53:58 -0600

    Ejercicio 07 resuelto

--- C7 Arrays/ejercicios/07.js
+++ C7 Arrays/ejercicios/07.js
@@ -2,6 +2,20 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+
+        array[j] = array[j + 1];
+        
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
 }
 
 module.exports = ordenarArray;
