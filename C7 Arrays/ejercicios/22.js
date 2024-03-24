a10a2317ad80d867ec6f1425fd67714b2d9aa616
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-24 12:44:14 -0600

    Ejercicio 22 resuelto

--- C7 Arrays/ejercicios/22.js
+++ C7 Arrays/ejercicios/22.js
@@ -2,6 +2,18 @@ function tablaDelSeis() {
   // Devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
   // Desde 0 al 60.
   // Tu código:
+
+  var tabla = [];
+
+  for( var i = 0; i <= 10; i++){
+    let numero = 0;
+    numero = 6 * i;
+    tabla.push(numero);
+  }
+
+  return tabla;
 }
 
+
+console.log(tablaDelSeis())
 module.exports = tablaDelSeis;
