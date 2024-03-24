915df0fa51f6b7553801e9c1669ff82900a3e610
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-24 14:15:34 -0600

    Ejercicio 25 resuelto

--- C7 Arrays/ejercicios/25.js
+++ C7 Arrays/ejercicios/25.js
@@ -3,6 +3,21 @@ function contarParesConContinue(numeros) {
   // Devuelve la cantidad de números pares que hay en el array.
   // Evita los impares utilizando continue.
   // Tu código:
+
+  var i = 0, count = 0;
+
+  while (i < numeros.length){
+    if (numeros[i] % 2 === 0){
+      count++;
+      i++;
+    }
+    else{
+      i++;
+      continue;
+    }
+  }
+
+  return count;
 }
 
 module.exports = contarParesConContinue;
