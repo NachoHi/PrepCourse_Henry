0d72672435890f1e06c735181667417b00448f2b
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-24 12:24:38 -0600

    Ejercicio 20 resuelto

--- C7 Arrays/ejercicios/20.js
+++ C7 Arrays/ejercicios/20.js
@@ -2,6 +2,20 @@ function todosIguales(array) {
   // Si todos los elementos del arreglo son iguales, retornar true.
   // Caso contrario, retornar false.
   // Tu código:
+
+  var count = 1;
+  for(var i = 0; i < array.length-1; i++){
+    if(array[i] === array[i+1]){
+      count++;
+    }
+  }
+
+  if (count === array.length){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 module.exports = todosIguales;
