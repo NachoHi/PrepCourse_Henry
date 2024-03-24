01790d4968e73c4ff96c0bbcd02f26aede8fbc5d
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-24 15:26:47 -0600

    Ejercicio 29 resuelto

--- C7 Arrays/ejercicios/29.js
+++ C7 Arrays/ejercicios/29.js
@@ -4,6 +4,23 @@ function encontrarNumeroFaltante(numeros) {
   // y retórnalo.
   // Devuelve null si el aray es vacío o si no hay números faltantes.
   // Tu código:
+
+  var faltante = false;
+  var j = 1;
+
+  if (numeros.length < 1){
+    return null;
+  }
+  else{
+    for (var i = 0; i < numeros.length; i++){
+      if(j !== numeros[i]){
+        return j;
+      }
+      j++;
+    }
+  }
+
+  return null;
 }
 
 module.exports = encontrarNumeroFaltante;
