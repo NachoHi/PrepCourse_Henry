50a5e146c3bc44f696089af8d4b2e6cf38df9e19
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-24 10:54:46 -0600

    Ejercicio 14 resuelto

--- C7 Arrays/ejercicios/14.js
+++ C7 Arrays/ejercicios/14.js
@@ -1,6 +1,16 @@
 function contarElementosMayoresA10(array) {
   // Cuenta y devuelve la cantidad de elementos mayores a 10 en el array de números.
   // Tu código:
+
+  var cont = 0;
+
+  for (var i = 0; i < array.length; i++){
+    if(array[i] > 10){
+      cont++;
+    }
+  }
+
+  return cont;
 }
 
 module.exports = contarElementosMayoresA10;
