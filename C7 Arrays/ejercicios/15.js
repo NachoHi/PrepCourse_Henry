5f7c88ed5285b576afabe29e2acc0a1bad90258e
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-24 11:42:18 -0600

    Ejercicio 15 resuelto

--- C7 Arrays/ejercicios/15.js
+++ C7 Arrays/ejercicios/15.js
@@ -2,6 +2,20 @@ function encontrarIndiceMayor(array) {
   // Encuentra el índice del número más grande en el array de números.
   // Devuelve el valor de este índice.
   // Tu código:
+
+  var indice = 0;
+
+  if (array.length > 1) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] > array[indice]) {
+        indice = i;
+      }
+    }
+    return indice;
+  } else {
+    return 0;
+  }
+  
 }
 
 module.exports = encontrarIndiceMayor;
