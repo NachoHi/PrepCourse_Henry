8bb74f02ecabd917f7a7d2ae96713ccc88f0d12f
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-25 14:40:10 -0600

    Ejercicio 06 resuelto

--- C8 Objects + Callbacks/objects/ejercicios/06.js
+++ C8 Objects + Callbacks/objects/ejercicios/06.js
@@ -2,6 +2,14 @@ const contarPropiedades = (objeto) => {
   // Cuenta y retorna el total de propiedades que tiene el objeto.
   // PISTA: Puedes iterarlo usando el bucle for-in.
   // Tu código:
+
+  var total = 0;
+
+  for(let propiedades in objeto){
+    total++;
+  }
+
+  return total;
 };
 
 module.exports = contarPropiedades;
