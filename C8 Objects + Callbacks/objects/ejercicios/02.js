0b1db0c2b69b1b84cb02c167d2cf158667e9247f
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-25 14:03:32 -0600

    Ejercicio 02 resuelto

--- C8 Objects + Callbacks/objects/ejercicios/02.js
+++ C8 Objects + Callbacks/objects/ejercicios/02.js
@@ -1,5 +1,5 @@
 function actualizarValorPropiedad(objeto, propiedad, valor) {
-   // Actualiza el valor de la propiedad del objeto recibidos en la función.
+   // Actualiza el valor de la propiedad del objeto recibidos enn la fución.
    // Retorna el objeto actualizado.
    // Tu código:
    objeto[propiedad] = valor;
