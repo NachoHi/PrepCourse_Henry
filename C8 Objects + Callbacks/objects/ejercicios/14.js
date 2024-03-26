9025c1999722ccab3cb814a284badc77bacdc5a7
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-25 18:52:01 -0600

    Ejercicio 14 resuelto

--- C8 Objects + Callbacks/objects/ejercicios/14.js
+++ C8 Objects + Callbacks/objects/ejercicios/14.js
@@ -1,7 +1,14 @@
 function tieneEmail(objetoUsuario) {
-  // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
+  // Verifica si el "ob,jetoUsuario" en su propiedad "email", posee un valor definido.
   // En ese caso, retorna true. Caso contrario, false.
   // Tu código:
+
+  if (objetoUsuario.email !== undefined && objetoUsuario.email !== null){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 module.exports = tieneEmail;
