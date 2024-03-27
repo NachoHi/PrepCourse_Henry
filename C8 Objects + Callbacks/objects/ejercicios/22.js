92cb07f394d67c3c4f0eb6cb3b585f64312e3f82
NachoHi <josei.nacho.ucv@gmail.com>
2024-03-27 09:04:10 -0600

    Ejercicio 22 resuelto

--- C8 Objects + Callbacks/objects/ejercicios/22.js
+++ C8 Objects + Callbacks/objects/ejercicios/22.js
@@ -5,6 +5,29 @@ function esAnagrama(str1, str2) {
    // IMPORTANTE: Un anagrama es una palabra que se forma
    // con las mismas letras que otra, pero en orden diferente.
    // Tu código:
+
+   var objeto1 = {};
+   var objeto2 = {};
+
+   if (str1.length !== str2.length){
+      return false;
+   }
+
+   for (let letra of str1){
+      objeto1[letra] = (objeto1[letra] || 0) + 1;
+   }
+
+   for (let letra of str2){
+      objeto2[letra] = (objeto2[letra] || 0) + 1;
+   }
+
+   for (let letra in str1){
+      if(objeto1[letra] !== objeto2[letra]){
+         return false;
+      }
+   }
+
+   return true;
 }
 
 module.exports = esAnagrama;
